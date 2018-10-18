75dac86994878c4413ef2140c5b297ff03196cb2
EduTel <15623704+EduTel@users.noreply.github.com>
2018-10-18 13:44:59 -0500

    cambios

--- color.py
+++ color.py
@@ -1,9 +1,34 @@
 #sudo apt install python-pip
 #pip install coloredlogs
+import os
 import sys
-print(sys.version)
-
 import coloredlogs, logging
+import colored_traceback
+from termcolor import colored
+from colorama import Fore, Back, Style
+
+import backtrace
+
+backtrace.hook(
+    reverse=True,
+    align=True,
+    strip_path=True,
+    enable_on_envvar_only=True,
+    on_tty=True,
+    conservative=True,
+    styles={
+        'backtrace': Fore.YELLOW + '{0}',
+        'error': Fore.RED + Style.BRIGHT + '{0}',
+        'line': Fore.RED + Style.BRIGHT + '{0}',
+        'module': '{0}',
+        'context': Style.BRIGHT + Fore.GREEN + '{0}',
+        'call': Fore.YELLOW + '--> ' + Style.BRIGHT + '{0}'
+    })
+
+
+colored_traceback.add_hook(always=True)
+
+
 
 # Create a logger object.
 logger = logging.getLogger(__name__)
@@ -27,6 +52,9 @@ logger.critical("this is a critical message")
 
 
 
-from termcolor import colored
 
-print colored('hello', 'red'), colored('world', 'green')
+
+print colored("""hello
+              hellow""", 'red'), colored('world', 'green')
+
+backtrace.unhook()
